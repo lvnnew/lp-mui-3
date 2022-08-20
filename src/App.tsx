4643def0be5f2bc7233de415d5313f41d2b5c62a
lvnnew <pirat_2-5@mail.ru>
2022-08-20 15:42:08 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Hero from "./components/Hero"
+import Header from "./components/Header"
+import Section from "./components/Section"
+import Testimonial from "./components/Testimonial"
+import ContactUs from "./components/ContactUs"
+import Footer from "./components/Footer"
+import AboutUs from "./components/AboutUs"
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider} from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Poppins',
+      'sans-serif',
+    ].join(','),
+  },});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Hero />
+      <Section />
+      <AboutUs />
+      <Testimonial />
+      <ContactUs />
+      <Footer />
+      </ThemeProvider>
+    </>
+
   );
 }
 
