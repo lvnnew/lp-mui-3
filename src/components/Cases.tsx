ecc1bbabc36c31101ecb7eb09ea6090227b6fa84
lvnnew <pirat_2-5@mail.ru>
2022-08-22 02:05:24 +0300

    Исправил импорты

--- src/components/Cases.tsx
+++ src/components/Cases.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography, Box, Button, Stack } from "@mui/material";
+import { Typography, Box, Button, Stack } from "@mui/material";
 
 const Cases = () => {
   return (
